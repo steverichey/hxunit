71ba8c2a05c5a52f70a49ce783f1f1e259178c72
polysemantic <polysemantic@d1f62a7c-4854-0410-9e74-7f21e379dd65>
2008-08-23 04:31:52 +0000

    git-svn-id: http://hxunit.googlecode.com/svn/trunk@22 d1f62a7c-4854-0410-9e74-7f21e379dd65

---
+++ Responder.hx
@@ -0,0 +1,9 @@
+﻿package hxunit.respond;
+
+import hxunit.TestStatus;
+
+interface Responder {
+	function execute(status : TestStatus) : Void;
+	function start() : Void;
+	function done() : Void;
+}
