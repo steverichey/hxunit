77d9beb18793de197a6033ccab7600daf14a590b
polysemantic <polysemantic@d1f62a7c-4854-0410-9e74-7f21e379dd65>
2008-08-23 12:18:07 +0000

    git-svn-id: http://hxunit.googlecode.com/svn/trunk@31 d1f62a7c-4854-0410-9e74-7f21e379dd65

---
+++ test/TeardownTest.hx
@@ -0,0 +1,27 @@
+﻿/**
+* ...
+* @author $(DefaultUser)
+*/
+
+package test;
+import hxunit.Assert;
+
+class TeardownTest {
+
+	public function new() {
+		
+	}
+
+	public function testFailTeardown() {
+		Assert.isTrue(true);
+	}
+	public function teardown() {
+		throw "fail";
+	}
+	
+	var async:Dynamic;
+	public function testFailAsyncTeardown() {
+		async = Assert.async(function() { Assert.isTrue(true); }, 1000);
+		async();
+	}
+}
