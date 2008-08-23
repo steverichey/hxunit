c2f0375365b4097bbb8f85207c4b230fdab14898
franco.ponticelli <franco.ponticelli@d1f62a7c-4854-0410-9e74-7f21e379dd65>
2008-08-23 10:38:49 +0000

    more tests for Assert
    git-svn-id: http://hxunit.googlecode.com/svn/trunk@27 d1f62a7c-4854-0410-9e74-7f21e379dd65

--- test/AssertTest.hx
+++ test/AssertTest.hx
@@ -20,8 +20,7 @@ class AssertTest {
 		var results = getResults();
 		restore();
 
-		for(i in 0...results.length)
-			Assert.equals(expected[i], results[i]);
+		compareResults(expected, results);
 	}
 
 	public function testIsFalse() {
@@ -34,8 +33,100 @@ class AssertTest {
 		var results = getResults();
 		restore();
 
+		compareResults(expected, results);
+	}
+
+	public function testIsNull() {
+		var tests    = [null, "string", 0.1,   0,     0.0,   -1,    this ];
+		var expected = [true, false,    false, false, false, false, false];
+
+		swap();
+		for(t in tests)
+			Assert.isNull(t);
+		var results = getResults();
+		restore();
+
+		compareResults(expected, results);
+	}
+
+	public function testNotNull() {
+		var tests    = [null,  "string", 0.1,  0,    0.0,  -1,   this];
+		var expected = [false, true,     true, true, true, true, true];
+
+		swap();
+		for(t in tests)
+			Assert.notNull(t);
+		var results = getResults();
+		restore();
+
+		compareResults(expected, results);
+	}
+
+	public function testIs() {
+		var tests    = ["string", 0.1,   1,    "string", 0.1,    0,    ];
+		var params   = [String,   Float, Int,  Int,      String, String];
+		var expected = [true,     true,  true, false,    false,  false ];
+
+		swap();
+		for(i in 0...tests.length)
+			Assert.is(tests[i], params[i]);
+		var results = getResults();
+		restore();
+
+		compareResults(expected, results);
+	}
+
+	public function testEquals() {
+		var tests    = ["string", "a",   1,    1,     0.1,  0.1,   null, null,  1   ];
+		var params   = ["string", "b",   1,    0,     0.1,  0.0,   null, 1,     1.0 ];
+		var expected = [true,     false, true, false, true, false, true, false, true];
+
+		swap();
+		for(i in 0...tests.length)
+			Assert.equals(tests[i], params[i]);
+		var results = getResults();
+		restore();
+
+		compareResults(expected, results);
+	}
+
+	public function testFloatEquals() {
+		var tests    = [1,    1,     0.1,  0.1,   1    ];
+		var params   = [1,    0,     0.1,  0.0,   1.0  ];
+		var expected = [true, false, true, false, true];
+
+		swap();
+		for(i in 0...tests.length)
+			Assert.floatEquals(tests[i], params[i]);
+		var results = getResults();
+		restore();
+
+		compareResults(expected, results);
+	}
+
+	public function testRaises() {
+		var fs = function() throw "a";
+		var fi = function() throw 1;
+		var tests    = [fs,      fi,      fs,     fi,   fs,    fi];
+		var params   = [Dynamic, Dynamic, String, Int,  Int,   String];
+		var expected = [true,    true,    true,   true, false, false ];
+
+		swap();
+		for(i in 0...tests.length)
+			Assert.raises(tests[i], params[i]);
+		var results = getResults();
+		restore();
+
+		compareResults(expected, results);
+	}
+
+	public function testAsync() {
+
+	}
+
+	function compareResults(expected : Array<Bool>, results : Array<Bool>) {
 		for(i in 0...results.length)
-			Assert.equals(expected[i], results[i]);
+			Assert.equals(expected[i], results[i], "failure at index #" + i);
 	}
 
 	function swap() {
